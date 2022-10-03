b30f3fa89847bc56ba3b15d3861c3bae725355f1
Nuzhah Kureemun <nuzk2904@gmail.com>
2022-10-03 13:36:38 +0400

    uncompleted stupid question

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root to: "pages#home"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+end
+# verb "url", to: "controller#action"
