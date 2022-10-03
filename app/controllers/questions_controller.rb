2c00b0d62ad0d9141a14ed36a33ff12515cb46de
Nuzhah Kureemun <nuzk2904@gmail.com>
2022-10-03 15:00:53 +0400

    stupid coaching completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:questions]
+    @answer = params[:question]
+    if params[:question].downcase == 'i am going to work right now!'
+
+      @answer = 'Great'
+    elsif params[:question].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work'
+    else
+      @answer = 'I dont care, get dressed and go to work'
+    end
   end
 end
